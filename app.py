6f05ce1b0f44c08f73082dff03ad2bfbeef16d5d
Kevin Harriss <special.kevin@gmail.com>
2013-04-24 10:39:52 -0500

    Refactored to hopefully make heroku happy.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from flask import Flask
 from flask import render_template
-from app import app
+
+app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("index.html")
